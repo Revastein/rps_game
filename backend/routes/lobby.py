b794b3d12db60c1d5981ffc12f840e731c9520b6
ian.kovzel <ian.kovzel@glera-games.com>
2025-03-01 15:37:11 +0400

    Update usage via lobby manager

--- backend/routes/lobby.py
+++ backend/routes/lobby.py
@@ -2,14 +2,12 @@ from typing import List
 
 from fastapi import APIRouter, HTTPException
 
+from backend.manager.lobby_manager import lobby_manager
 from backend.models.database import Session, UserOrm
 from backend.models.schemas import GameResult, Lobby, Queue
 from backend.service.lobby_service import LobbyView
 
 router = APIRouter(prefix="/v1", tags=["Lobby"])
-
-queue = []
-lobbies = []
 
 
 @router.post("/join_queue", response_model=Queue, status_code=201)
@@ -18,154 +16,159 @@ async def join_queue(game_queue: Queue):
         user = await session.get(UserOrm, game_queue.user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
-    for user in queue:
-        if user.user_id == game_queue.user_id:
-            raise HTTPException(status_code=400, detail="User already in the queue")
-    for lobby in lobbies:
-        for player in lobby.players:
-            if player.user_id == game_queue.user_id:
-                raise HTTPException(status_code=400, detail="User already in a lobby")
 
-    user_to_queue = Queue(user_id=game_queue.user_id)
-    queue.append(user_to_queue)
-    await LobbyView.check_queue(queue, lobbies)
-    return user_to_queue
+    if lobby_manager.is_user_in_queue(game_queue.user_id):
+        raise HTTPException(status_code=400, detail="User already in the queue")
+    if lobby_manager.is_user_in_lobby(game_queue.user_id):
+        raise HTTPException(status_code=400, detail="User already in a lobby")
+
+    lobby_manager.add_to_queue(game_queue.user_id)
+    await LobbyView.check_queue()
+    return game_queue
 
 
 @router.get("/get_queue", response_model=List[Queue], status_code=200)
 async def get_queue():
-    return queue
+    return lobby_manager.queue
 
 
-@router.delete("/delete_user_from_queue", response_model=None, status_code=204)
+@router.delete("/delete_user_from_queue", response_model=dict, status_code=200)
 async def delete_user_from_queue(game_queue: Queue):
-    for user in queue:
-        if user.user_id == game_queue.user_id:
-            queue.remove(user)
-            return {"message": "Successfully left the queue"}
+    if lobby_manager.remove_user_from_queue(game_queue.user_id):
+        return {"message": "Successfully left the queue"}
     return {"message": "User not found in the queue"}
 
 
 @router.get("/get_all_lobbies", response_model=List[Lobby], status_code=200)
 async def get_all_lobbies():
-    return lobbies
-
-
-@router.get("/get_lobby_id_by_user/{user_id}", response_model=str, status_code=200)
-async def get_lobby_id_by_user(user_id: int):
-    for lobby in lobbies:
-        for player in lobby.players:
-            if player.user_id == user_id:
-                return lobby.lobby_id
-    raise HTTPException(status_code=404, detail="Lobby not found for this user")
+    return lobby_manager.lobbies
 
 
 @router.get("/get_lobby/{lobby_id}", response_model=Lobby, status_code=200)
 async def get_lobby(lobby_id: str):
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            return lobby
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if lobby:
+        return lobby
     raise HTTPException(status_code=404, detail="Lobby not found")
-
-
-@router.get("/get_lobby_by_user/{user_id}", response_model=Lobby, status_code=200)
-async def get_lobby_by_user(user_id: int):
-    for lobby in lobbies:
-        for player in lobby.players:
-            if player.user_id == user_id:
-                return lobby
-    raise HTTPException(status_code=404, detail="Lobby not found for this user")
 
 
 @router.post("/make_choice/{lobby_id}", response_model=GameResult, status_code=200)
 async def make_choice(lobby_id: str, current_user_id: int, choice: str) -> dict:
     valid_choices = ["rock", "paper", "scissors"]
     if choice not in valid_choices:
-        raise HTTPException(status_code=400, detail="Invalid choice. Must be 'rock', 'paper', or 'scissors'.")
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid choice. Must be 'rock', 'paper', or 'scissors'.",
+        )
 
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            for player in lobby.players:
-                if player.user_id == current_user_id:
-                    player.choice = choice
-            break
-    else:
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if not lobby:
         return {"winner": "waiting"}
 
-    if all(player.choice is not None for player in lobby.players):
+    lobby_manager.set_player_choice(lobby, current_user_id, choice)
+
+    if lobby_manager.is_lobby_ready(lobby):
         player1_choice = lobby.players[0].choice
         player2_choice = lobby.players[1].choice
-        result = LobbyView.determine_winner(player1_choice, player2_choice)
-        if result != "waiting":
-            if result == "tie":
-                await LobbyView.update_player_stats(lobby.players[0].user_id, result, lobby.players[1].user_id)
-            else:
-                winner_id = lobby.players[0].user_id if result == "player1" else lobby.players[1].user_id
-                loser_id = lobby.players[1].user_id if result == "player1" else lobby.players[0].user_id
-                await LobbyView.update_player_stats(winner_id, result, loser_id)
-        return {"winner": result}
+        result = LobbyView.determine_winner(
+            player1_choice,
+            player2_choice,
+            lobby.players[0].user_id,
+            lobby.players[1].user_id,
+        )
+        if result == "tie":
+            await LobbyView.update_player_stats(
+                lobby.players[0].user_id, result, lobby.players[1].user_id
+            )
+            return {"winner": "tie"}
+        else:
+            winner_id = int(result)
+            loser_id = (
+                lobby.players[1].user_id
+                if winner_id == lobby.players[0].user_id
+                else lobby.players[0].user_id
+            )
+            await LobbyView.update_player_stats(winner_id, "win", loser_id)
+            async with Session() as session:
+                winner = await session.get(UserOrm, winner_id)
+                if not winner:
+                    raise HTTPException(status_code=404, detail="Winner not found")
+
+            lobby.winner_id = winner_id
+            lobby.winner_username = winner.username
+
+            return {"winner_id": winner_id, "login": winner.username}
     return {"winner": "waiting"}
 
 
-@router.post("/surrender_game/{lobby_id}/{player_id}", response_model=GameResult, status_code=200)
+@router.post(
+    "/surrender_game/{lobby_id}/{player_id}", response_model=GameResult, status_code=200
+)
 async def surrender_game(lobby_id: str, player_id: int):
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            for player in lobby.players:
-                if player.user_id == player_id:
-                    opponent_id = [p.user_id for p in lobby.players if p.user_id != player_id][0]
-                    await LobbyView.update_player_stats_on_surrender(player_id, opponent_id)
-                    return {"winner": str(opponent_id)}
-    raise HTTPException(status_code=404, detail="Lobby or player not found")
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if not lobby:
+        raise HTTPException(status_code=404, detail="Lobby not found")
+    if player_id not in [player.user_id for player in lobby.players]:
+        raise HTTPException(status_code=404, detail="Player not found in lobby")
+    opponent_ids = [p.user_id for p in lobby.players if p.user_id != player_id]
+    if not opponent_ids:
+        raise HTTPException(status_code=400, detail="No opponent found")
+    opponent_id = opponent_ids[0]
+    await LobbyView.update_player_stats_on_surrender(player_id, opponent_id)
+    return {"winner": str(opponent_id)}
 
 
 @router.get("/check_winner/{lobby_id}", response_model=GameResult, status_code=200)
 async def check_winner(lobby_id: str) -> dict:
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            if all(player.choice is not None for player in lobby.players):
-                player1_choice = lobby.players[0].choice
-                player2_choice = lobby.players[1].choice
-                result = LobbyView.determine_winner(player1_choice, player2_choice)
-                if result != "waiting":
-                    return {"winner": result}
-                else:
-                    return {"winner": "waiting"}
-    raise HTTPException(status_code=404, detail="Lobby not found")
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if not lobby:
+        raise HTTPException(status_code=404, detail="Lobby not found")
+    if lobby_manager.is_lobby_ready(lobby):
+        player1_choice = lobby.players[0].choice
+        player2_choice = lobby.players[1].choice
+        result = LobbyView.determine_winner(
+            player1_choice,
+            player2_choice,
+            lobby.players[0].user_id,
+            lobby.players[1].user_id,
+        )
+        return {"winner": result}
+    return {"winner": "waiting"}
 
 
-@router.post("/confirm_rematch/{lobby_id}/{player_id}", response_model=None, status_code=200)
+@router.post(
+    "/confirm_rematch/{lobby_id}/{player_id}", response_model=None, status_code=200
+)
 async def confirm_rematch(lobby_id: str, player_id: int, rematch_accepted: bool):
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            if player_id in [player.user_id for player in lobby.players]:
-                if rematch_accepted:
-                    if player_id not in lobby.rematch_accepted:
-                        lobby.rematch_accepted.append(player_id)
-                else:
-                    if player_id in lobby.rematch_accepted:
-                        lobby.rematch_accepted.remove(player_id)
-                if len(lobby.rematch_accepted) == 2:
-                    await restart_lobby(lobby_id)
-                return
-    raise HTTPException(status_code=404, detail="Lobby or player not found")
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if not lobby:
+        raise HTTPException(status_code=404, detail="Lobby not found")
+    if player_id not in [player.user_id for player in lobby.players]:
+        raise HTTPException(status_code=404, detail="Player not found in lobby")
+    if rematch_accepted:
+        if player_id not in lobby.rematch_accepted:
+            lobby.rematch_accepted.append(player_id)
+    else:
+        if player_id in lobby.rematch_accepted:
+            lobby.rematch_accepted.remove(player_id)
+    if len(lobby.rematch_accepted) == len(lobby.players):
+        await restart_lobby(lobby_id)
+    return
 
 
 @router.post("/restart_lobby/{lobby_id}", response_model=Lobby, status_code=200)
 async def restart_lobby(lobby_id: str):
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            for player in lobby.players:
-                player.choice = None
-            lobby.rematch_accepted = []
-            return lobby
-    raise HTTPException(status_code=404, detail="Lobby not found")
+    lobby = lobby_manager.get_lobby(lobby_id)
+    if not lobby:
+        raise HTTPException(status_code=404, detail="Lobby not found")
+    for player in lobby.players:
+        player.choice = None
+    lobby.rematch_accepted = []
+    return lobby
 
 
-@router.delete("/delete_lobby/{lobby_id}", response_model=None, status_code=204)
+@router.delete("/delete_lobby/{lobby_id}", response_model=dict, status_code=200)
 async def delete_lobby(lobby_id: str):
-    for lobby in lobbies:
-        if lobby.lobby_id == lobby_id:
-            lobbies.remove(lobby)
-            return {"message": "Lobby successfully deleted"}
+    if lobby_manager.remove_lobby(lobby_id):
+        return {"message": "Lobby successfully deleted"}
     return {"message": "Lobby not found"}
