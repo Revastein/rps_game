0a142eafc60f5e856ab1626477d2206cb23762f4
ian.kovzel <ian.kovzel@glera-games.com>
2025-03-01 15:37:21 +0400

    Update usage via lobby manager

--- backend/service/lobby_service.py
+++ backend/service/lobby_service.py
@@ -1,53 +1,49 @@
-import uuid
-
 from fastapi import HTTPException
 
+from backend.manager.lobby_manager import lobby_manager
 from backend.models.database import Session, UserOrm
-from backend.models.schemas import LobbyPlayer, Lobby
 
 
 class LobbyView:
     @classmethod
-    async def check_queue(cls, queue, lobbies):
-        if len(queue) >= 2:
-            lobby_players = [LobbyPlayer(user_id=user.user_id) for user in queue[:2]]
-            lobby_id = str(uuid.uuid4())
-            lobby = Lobby(lobby_id=lobby_id, players=lobby_players)
-            lobbies.append(lobby)
-            del queue[:2]
+    async def check_queue(cls):
+        lobby_manager.create_lobby_from_queue()
 
     @classmethod
-    def determine_winner(cls, choice1: str, choice2: str) -> str:
-        if choice1 == choice2:
+    def determine_winner(
+        cls, player1_choice: str, player2_choice: str, player1_id: int, player2_id: int
+    ) -> str:
+        if player1_choice == player2_choice:
             return "tie"
-        elif (choice1 == "rock" and choice2 == "scissors") or \
-                (choice1 == "scissors" and choice2 == "paper") or \
-                (choice1 == "paper" and choice2 == "rock"):
-            return "player1"
+        elif (
+            (player1_choice == "rock" and player2_choice == "scissors")
+            or (player1_choice == "scissors" and player2_choice == "paper")
+            or (player1_choice == "paper" and player2_choice == "rock")
+        ):
+            return str(player1_id)
         else:
-            return "player2"
+            return str(player2_id)
 
     @classmethod
-    async def update_player_stats(cls, user_id: int, result: str, opponent_id: int) -> None:
+    async def update_player_stats(
+        cls, user_id: int, result: str, opponent_id: int
+    ) -> None:
         async with Session() as session:
             player = await session.get(UserOrm, user_id)
             if not player:
                 raise HTTPException(status_code=404, detail="Player not found")
 
-            if result == "player1":
-                player.rating += 5
-                player.wins += 1
-                opponent = await session.get(UserOrm, opponent_id)
-                opponent.losses += 1
-            elif result == "player2":
-                player.rating += 5
-                player.wins += 1
-                opponent = await session.get(UserOrm, opponent_id)
-                opponent.losses += 1
-            else:
+            opponent = await session.get(UserOrm, opponent_id)
+            if not opponent:
+                raise HTTPException(status_code=404, detail="Opponent not found")
+
+            if result == "tie":
                 player.ties += 1
-                opponent = await session.get(UserOrm, opponent_id)
                 opponent.ties += 1
+            else:
+                player.rating += 5
+                player.wins += 1
+                opponent.losses += 1
 
             player.games_played += 1
             opponent.games_played += 1
@@ -55,12 +51,16 @@ class LobbyView:
             await session.commit()
 
     @classmethod
-    async def update_player_stats_on_surrender(cls, player_id: int, opponent_id: int) -> None:
+    async def update_player_stats_on_surrender(
+        cls, player_id: int, opponent_id: int
+    ) -> None:
         async with Session() as session:
             player = await session.get(UserOrm, player_id)
             opponent = await session.get(UserOrm, opponent_id)
             if not player or not opponent:
-                raise HTTPException(status_code=404, detail="Player or opponent not found")
+                raise HTTPException(
+                    status_code=404, detail="Player or opponent not found"
+                )
 
             player.wins += 1
             player.rating += 5
